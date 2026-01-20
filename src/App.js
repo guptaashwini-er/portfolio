de5523493de07e91f52b1813e4d2f5c568a8218f
guptaashwini-er <tanish0007yt@gmail.com>
2026-01-20 21:37:51 +0530

    as

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
-import Project from './component/project/Project';
-import Hero from './component/home/Hero';
+
 import Main from './route';
 import Footer from './Footer';
 
